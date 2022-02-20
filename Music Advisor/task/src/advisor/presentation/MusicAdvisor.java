1350c1986d6c57d19b6177c76933d5ea2facec37
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2022-02-20 18:12:20 +0100

    Completed - Stage 2/5: Rocking OAuth

--- Music Advisor/task/src/advisor/presentation/MusicAdvisor.java
+++ Music Advisor/task/src/advisor/presentation/MusicAdvisor.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 
 public class MusicAdvisor {
     private final Scanner scanner;
+    private boolean accessGranted = false;
+    private final String CLIENT_ID = "b68e796f69654238a1631d9a19c67898";
+    private final String CLIENT_SECRET = "4868231c3e094fc5b83fe6738040c85a";
+    private final String REDIRECT_URI = "http://localhost:8080";
 
     public MusicAdvisor() {
         scanner = new Scanner(System.in);
@@ -37,6 +41,11 @@ public class MusicAdvisor {
                 String category = scanner.next();
                 playlist(category);
                 break;
+            case "auth":
+                //String auth = scanner.next();
+                String auth = String.format("https://accounts.spotify.com/authorize?client_id=%s&redirect_uri=%s&response_type=code", CLIENT_ID, REDIRECT_URI);
+                auth(auth);
+                break;
             case "exit":
                 System.out.println("---GOODBYE!---");
                 System.exit(0);
@@ -45,21 +54,44 @@ public class MusicAdvisor {
     }
 
     public void featured() {
+        if (!accessGranted){
+            System.out.println("Please, provide access for application.");
+            return;
+        }
         System.out.println("---FEATURED---");
         System.out.println("Wake Up and Smell the Coffee");
     }
     public void newSelected() {
+        if (!accessGranted){
+            System.out.println("Please, provide access for application.");
+            return;
+        }
         System.out.println("---NEW RELEASES---");
         System.out.println("Mountains [Sia, Diplo, Labrinth]");
     }
     public void categories() {
+        if (!accessGranted){
+            System.out.println("Please, provide access for application.");
+            return;
+        }
         System.out.println("---CATEGORIES---");
         System.out.println("Mood");
         System.out.println("Pop");
     }
     public void playlist(String cat) {
+        if (!accessGranted){
+            System.out.println("Please, provide access for application.");
+            return;
+        }
         System.out.printf("---%s PLAYLISTS---\n", cat.toUpperCase(Locale.ROOT));
         System.out.println("Mood");
     }
+
+    public void auth(String authUri){
+        System.out.println(authUri);
+        accessGranted = true;
+        System.out.println("---SUCCESS---");
+    }
+
 
 }
